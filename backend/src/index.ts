2a32ee102dd22b4e0dacd6dc5f877040af6e86ef
Avashneupane9857 <juisch20220011252@gmail.com>
2024-11-15 08:05:58 +0545

    feat: routing

---
+++ backend/src/index.ts
@@ -0,0 +1,30 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+app.post('api/v1/user/signup',(c)=>{
+  return c.text("hello from signup")
+})
+app.post('api/v1/user/signin',(c)=>{
+  return c.text("hello from signin")
+})
+app.post('api/v1/blog',(c)=>{
+  return c.text("hello from blog")
+})
+app.put('api/v1/blog',(c)=>{
+  return c.text("hello from update blog")
+})
+app.get('api/v1/blog/:id',(c)=>{
+  return c.text("hello from get blog")
+})
+app.get('api/v1/blog/bulk',(c)=>{
+  return c.text("hello from bulk blog")
+})
+
+
+
+
+export default app
